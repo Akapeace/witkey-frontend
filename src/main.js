ba53742536107eb4176b99c5db0e29e6ebc3a13d
peace <wjppeace@163.com>
2026-04-01 18:38:21 +0800

    add - 使用 pinia-persist 插件实现 Pinia 数据持久化

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import 'animate.css'
 import 'nprogress/nprogress.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// 引入全局状态管理 Pinia
+import pinia from '@/stores'
 
 import App from '@/App.vue'
 import router from '@/router'
@@ -12,13 +13,13 @@ import router from '@/router'
 import '@/permission'
 
 
-const pinia = createPinia()
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(pinia)
+
 app.use(router)
+app.use(pinia)
 app.mount('#app')
