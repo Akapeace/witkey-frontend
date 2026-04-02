ae0bdc8486524577182ac617cf88fe34a25ecb6c
peace <wjppeace@163.com>
2026-04-02 12:00:52 +0800

    add - 用户修改密码、退出登录功能开发

--- src/composables/util.js
+++ src/composables/util.js
@@ -3,9 +3,9 @@ import nprogress from "nprogress"
 // 消息提示
 export function showMessage(message = '提示内容', type = 'success', customClass = '') {
   return ElMessage({
-      type: type,
-      message,
-      customClass,
+    type: type,
+    message,
+    customClass,
   })
 }
 
@@ -17,4 +17,16 @@ export function showPageLoading() {
 // 隐藏页面加载 Loading
 export function hidePageLoading() {
   nprogress.done()
+}
+
+export function showModel(content = '提示内容', type = 'warning', title = '') {
+  return ElMessageBox.confirm(
+    content,
+    title,
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type,
+    }
+  )
 }
